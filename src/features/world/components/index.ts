22378017f4d8a745bb79cad2b441cc0ef2dd7dca
jamlove1318-lab <jamlove1318@gmail.com>
2026-09-02 00:18:34 +0530

    feat(ui): export world depth primitives

--- src/features/world/components/index.ts
+++ src/features/world/components/index.ts
@@ -2,3 +2,4 @@ export * from './LivingWorldUI';
 export * from './LivingWorldActors';
 export * from './LivingWorldSurface';
 export * from './LivingWorldScene';
+export * from './LivingWorldParallax';
